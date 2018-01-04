1c4f485e6cbd4e5a9f2ad9e23ff0bd972ef5133b
fmaury <fab.maury@gmail.com>
2018-01-04 14:28:05 +0100

    Feature: add login page

--- src/login/Login.js
+++ src/login/Login.js
@@ -1,24 +1,62 @@
 import React, { Component } from 'react';
 import Rating from 'react-rating';
-import { Grid, Col, Row } from 'react-bootstrap';
+import { Grid, Col, Row, FormGroup, ControlLabel, FormControl, Button, HelpBlock } from 'react-bootstrap';
+
+const LoginDivStyle = {
+	align: 'center',
+	border: '1px solid #ededed',
+	borderRadius: '3px',
+	boxShadow: '0 1px 3px rgba(0,0,0,.25)',
+	backgroundColor: '#fff',
+	padding: '10px',
+	position: 'relative',
+	paddingTop: '1.75em',
+	maxWidth:'500px'
+
+};
+
+
+
+function FieldGroup({ id, label, help, ...props }) {
+	return (
+		<FormGroup controlId={id} >
+			<ControlLabel>{label}</ControlLabel>
+			<FormControl {...props} />
+			{help && <HelpBlock>{help}</HelpBlock>}
+		</FormGroup>
+	);
+}
 
 class Login extends Component {
+
 
 	render () {
 		const self = this;
 		return (
-			<form action="/api/login" method="post">
-				<div class="form-group">
-					<label for="username">UserName: </label>
-					<input type="text" class="form-control" id="username" name="username"/>
-				</div>
-				<div class="form-group">
-					<label for="pwd">Password:</label>
-					<input type="password" class="form-control" id="pwd" name="password"/>
-				</div>
-				<button type="submit" class="btn btn-success">Submit</button>
-				<input type="hidden" name="${_csrf.parameterName}" value="${_csrf.token}" />
-			</form>)
+			//<div style={LoginDivStyle}>
+				<form action="/api/login" method="post">
+					<Grid style={LoginDivStyle} >
+					<Col >
+					<FieldGroup
+						id="formControlsText"
+						type="login"
+						label="Identifiant"
+						placeholder="Prénom Nom"
+						/>
+					<FieldGroup
+						id="formControlsEmail"
+						type="password"
+						label="Mot de passe"
+						placeholder="Le même que sur l'ensemble des outils Arolla"
+						/>
+
+					<Button bsStyle="primary" type="submit">
+						Me connecter
+					</Button>
+					</Col>
+						</Grid>
+				</form>//</div>
+		 )
 	}
 }
 
